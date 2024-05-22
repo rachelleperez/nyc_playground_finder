2c2862fccb78082635a3f7ad88d37bfcb0233e3a
Rachelle Perez <rachelleaperez@gmail.com>
2024-05-22 11:05:50 -0400

    popup to BEM

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,11 +4,11 @@ import "./Popup.css";
 const Popup = ({ message, onClose }) => {
   return (
     <div className="popup">
-      <div className="popup-content">
+      <div className="popup__content">
         <img
           src="/images/Cross.svg"
           alt="Close"
-          className="popup-close-button"
+          className="popup__close-button"
           onClick={onClose}
         />
         <p>{message}</p>
