d06121eae7498d7d556f7ce94ad044a8c7f35af6
Rachelle Perez <rachelleaperez@gmail.com>
2024-05-22 21:48:32 -0400

    hashrouter over browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ParkDetails from "./pages/ParkDetails/ParkDetails";
 import About from "./pages/About/About";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "normalize.css";
